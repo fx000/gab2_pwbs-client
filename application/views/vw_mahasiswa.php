ce2fa15b158907c153fa8103e9b0a2ad759459fb
Fredy Hermawan <fredihermawan1211@gmail.com>
2022-10-28 13:29:19 +0700

    SETUP PROJECT

---
+++ application/views/vw_mahasiswa.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tampil Data Mahasiswa</title>
+    <link rel="stylesheet" href="<?php echo base_url("ext/style.css")?>">
+</head>
+
+<body>
+    <!-- Buat Area Menu -->
+    <nav>
+
+    </nav>
+
+    <!-- Buat Area Tabel -->
+    <table>
+        <!-- Header Tabel -->
+        <thead>
+            <tr>
+                <th style="width: 10%;">Aksi</th>
+                <th style="width: 5%;">No</th>
+                <th style="width: 10%;">NPM</th>
+                <th style="width: auto;">Nama</th>
+                <th style="width: 15%;">Teleopon</th>
+                <th style="width: 10%;">Jurusan</th>
+            </tr>
+        </thead>
+        <!-- Isi Tabel -->
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
